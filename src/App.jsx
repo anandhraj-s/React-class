a94bae5789336e76f45f9fa74e198c601f37c3da
anandharaj-s <anandharajsrjpm@gmail.com>
2023-10-19 15:04:39 +0100

    task 22 html

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,59 @@
  import React from 'react'
- 
+ import'./app.css'
  export default function App() {
    return (
-     <div>App</div>
+     <div class = "container">
+       <div class="card">
+         <label>FREE</label>
+         <h3>$0/month</h3>
+         <ul >
+           <li>single user</li>
+           <li>500GB Storage</li>
+           <li>Unlimited Public Projects</li>
+           <li>Community Acces</li>
+           <li>Unlimited Private Projects </li>
+           <li>Dedicate Phone Support</li>
+           <li>Free Subdomain</li>
+           <li>Monthly Status Report</li>
+         </ul>
+         <button>Button</button>
+         
+       </div>
+
+       <div class="card">
+         <label>PLUS</label>
+         <h3>$9/month</h3>
+         <ul>
+           <li>5user</li>
+           <li>500GB Storage</li>
+           <li>Unlimited Public Projects</li>
+           <li>Community Acces</li>
+           <li>Unlimited Private Projects </li>
+           <li>Dedicate Phone Support</li>
+           <li>Free Subdomain</li>
+           <li>Monthly Status Report</li>
+         </ul>
+         <button>Button</button>
+         
+       </div>
+
+       <div class="card">
+         <label>PRO</label>
+         <h3>$0/month</h3>
+         <ul>
+           <li>single user</li>
+           <li>500GB Storage</li>
+           <li>Unlimited Public Projects</li>
+           <li>Community Acces</li>
+           <li>Unlimited Private Projects </li>
+           <li>Dedicate Phone Support</li>
+           <li>Free Subdomain</li>
+           <li>Monthly Status Report</li>
+         </ul>
+         <button>Button</button>
+         
+       </div>
+
+     </div>
    )
  }
- 
